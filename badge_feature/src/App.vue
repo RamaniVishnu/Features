280c28070294e9587ea50d362e37976b04d0f0ba
Ramani V <86917886+RamaniVishnu@users.noreply.github.com>
2023-07-28 21:34:30 +0530

    Badge-feature in vuejs

---
+++ badge_feature/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <BadgeComp />
+  </div>
+</template>
+
+<script>
+import BadgeComp from './components/BadgeComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BadgeComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
