77432bb8fffb8f6638da06d024d58d63619da031
Ramani V <86917886+RamaniVishnu@users.noreply.github.com>
2024-04-02 10:20:09 +0530

    Create Timer.vue

---
+++ Timer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="increment">start</button>
+    <button @click="pauseVal">pause</button>
+    <button @click="resetCount">Reset</button>
+    Timer: {{ this.count }}
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+  return{
+    count: 0,
+    storeVal: 0,
+    clearTime: null
+  }
+},
+methods:{
+  increment(){
+    this.clearTime =setInterval(()=>{
+      console.log('increment....');
+      this.count++},1000);
+  },
+  pauseVal(){
+    this.storeVal = this.count;
+    clearInterval(this.clearTime);
+  },
+  resetCount(){
+    this.count = 0;
+  }
+
+}
+}
+</script>
+
+<style>
+</style>
